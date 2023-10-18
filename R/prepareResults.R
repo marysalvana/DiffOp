5f3a96fcd871904b71a9f577b90b5c5eea2221bd
geostatistech <yourlainess@gmail.com>
2023-10-18 13:14:17 +0300

    added feature where scale horizontal can be fixed and only scale vertical is estimated

--- R/prepareResults.R
+++ R/prepareResults.R
@@ -419,6 +419,210 @@ prepareResults_mle <- function(fittedModel,
 
       }
 
+    }else if(scale_horizontal_fix & !scale_vertical_fix){
+
+      if(a1_fix & b1_fix & a2_fix & b2_fix){
+
+        if(c1_fix & c2_fix){
+
+          if(d1_fix & d2_fix){
+
+            results <- list(convergence_code = fittedModel$code, iterations = fittedModel$iterations, loglikelihood_value = -fittedModel$minimum, theta = fittedModel$estimate, gradient = fittedModel$gradient,
+                            est_beta = BETA,
+                            est_beta_sd = est_sd[1],
+                            est_scale_horizontal = SCALE_HORIZONTAL,
+                            est_scale_vertical = SCALE_VERTICAL,
+                            est_scale_vertical_sd = est_sd[2],
+                            est_a1 = A1,
+                            est_b1 = B1,
+                            est_c1_coef = C1_coef,
+                            est_d1 = D1,
+                            est_a2 = A2,
+                            est_b2 = B2,
+                            est_c2_coef = C2_coef,
+                            est_d2 = D2,
+                            splines_degree = splines_degree, inner_knots1 = inner_knots1, inner_knots2 = inner_knots2)
+
+          }else if(!d1_fix & !d2_fix){
+
+            results <- list(convergence_code = fittedModel$code, iterations = fittedModel$iterations, loglikelihood_value = -fittedModel$minimum, theta = fittedModel$estimate, gradient = fittedModel$gradient,
+                            est_beta = BETA,
+                            est_beta_sd = est_sd[1],
+                            est_scale_horizontal = SCALE_HORIZONTAL,
+                            est_scale_vertical = SCALE_VERTICAL,
+                            est_scale_vertical_sd = est_sd[2],
+                            est_a1 = A1,
+                            est_b1 = B1,
+                            est_c1_coef = C1_coef,
+                            est_d1 = D1,
+                            est_d1_sd = est_sd[3],
+                            est_a2 = A2,
+                            est_b2 = B2,
+                            est_c2_coef = C2_coef,
+                            est_d2 = D2,
+                            est_d2_sd = est_sd[4],
+                            splines_degree = splines_degree, inner_knots1 = inner_knots1, inner_knots2 = inner_knots2)
+
+          }
+
+        }else if(!c1_fix & !c2_fix){
+
+          if(d1_fix & d2_fix){
+
+            results <- list(convergence_code = fittedModel$code, iterations = fittedModel$iterations, loglikelihood_value = -fittedModel$minimum, theta = fittedModel$estimate, gradient = fittedModel$gradient,
+                            est_beta = BETA,
+                            est_beta_sd = est_sd[1],
+                            est_scale_horizontal = SCALE_HORIZONTAL,
+                            est_scale_vertical = SCALE_VERTICAL,
+                            est_scale_vertical_sd = est_sd[2],
+                            est_a1 = A1,
+                            est_b1 = B1,
+                            est_c1_coef = C1_coef,
+                            est_c1_coef_sd = est_sd[2 + 1:nb1],
+                            est_d1 = D1,
+                            est_a2 = A2,
+                            est_b2 = B2,
+                            est_c2_coef = C2_coef,
+                            est_c2_coef_sd = est_sd[2 + nb1 + 1:nb2],
+                            est_d2 = D2,
+                            splines_degree = splines_degree, inner_knots1 = inner_knots1, inner_knots2 = inner_knots2)
+
+          }else if(!d1_fix & !d2_fix){
+
+            results <- list(convergence_code = fittedModel$code, iterations = fittedModel$iterations, loglikelihood_value = -fittedModel$minimum, theta = fittedModel$estimate, gradient = fittedModel$gradient,
+                            est_beta = BETA,
+                            est_beta_sd = est_sd[1],
+                            est_scale_horizontal = SCALE_HORIZONTAL,
+                            est_scale_vertical = SCALE_VERTICAL,
+                            est_scale_vertical_sd = est_sd[2],
+                            est_a1 = A1,
+                            est_b1 = B1,
+                            est_c1_coef = C1_coef,
+                            est_c1_coef_sd = est_sd[2 + 1:nb1],
+                            est_d1 = D1,
+                            est_d1_sd = est_sd[2 + nb1 + 1],
+                            est_a2 = A2,
+                            est_b2 = B2,
+                            est_c2_coef = C2_coef,
+                            est_c2_coef_sd = est_sd[2 + nb1 + 1 + 1:nb2],
+                            est_d2 = D2,
+                            est_d2_sd = est_sd[2 + nb1 + 1 + nb2 + 1],
+                            splines_degree = splines_degree, inner_knots1 = inner_knots1, inner_knots2 = inner_knots2)
+
+          }
+
+        }
+
+      }else if(!a1_fix & !b1_fix & !a2_fix & !b2_fix){
+
+        if(c1_fix & c2_fix){
+
+          if(d1_fix & d2_fix){
+
+            results <- list(convergence_code = fittedModel$code, iterations = fittedModel$iterations, loglikelihood_value = -fittedModel$minimum, theta = fittedModel$estimate, gradient = fittedModel$gradient,
+                            est_beta = BETA,
+                            est_beta_sd = est_sd[1],
+                            est_scale_horizontal = SCALE_HORIZONTAL,
+                            est_scale_vertical = SCALE_VERTICAL,
+                            est_scale_vertical_sd = est_sd[2],
+                            est_a1 = A1,
+                            est_a1_sd = est_sd[3],
+                            est_b1 = B1,
+                            est_b1_sd = est_sd[4],
+                            est_c1_coef = C1_coef,
+                            est_d1 = D1,
+                            est_a2 = A2,
+                            est_a2_sd = est_sd[5],
+                            est_b2 = B2,
+                            est_b2_sd = est_sd[6],
+                            est_c2_coef = C2_coef,
+                            est_d2 = D2,
+                            splines_degree = splines_degree, inner_knots1 = inner_knots1, inner_knots2 = inner_knots2)
+
+          }else if(!d1_fix & !d2_fix){
+
+            results <- list(convergence_code = fittedModel$code, iterations = fittedModel$iterations, loglikelihood_value = -fittedModel$minimum, theta = fittedModel$estimate, gradient = fittedModel$gradient,
+                            est_beta = BETA,
+                            est_beta_sd = est_sd[1],
+                            est_scale_horizontal = SCALE_HORIZONTAL,
+                            est_scale_vertical = SCALE_VERTICAL,
+                            est_scale_vertical_sd = est_sd[2],
+                            est_a1 = A1,
+                            est_a1_sd = est_sd[3],
+                            est_b1 = B1,
+                            est_b1_sd = est_sd[4],
+                            est_c1_coef = C1_coef,
+                            est_d1 = D1,
+                            est_d1_sd = est_sd[5],
+                            est_a2 = A2,
+                            est_a2_sd = est_sd[6],
+                            est_b2 = B2,
+                            est_b2_sd = est_sd[7],
+                            est_c2_coef = C2_coef,
+                            est_d2 = D2,
+                            est_d2_sd = est_sd[8],
+                            splines_degree = splines_degree, inner_knots1 = inner_knots1, inner_knots2 = inner_knots2)
+
+          }
+
+        }else if(!c1_fix & !c2_fix){
+
+          if(d1_fix & d2_fix){
+
+            results <- list(convergence_code = fittedModel$code, iterations = fittedModel$iterations, loglikelihood_value = -fittedModel$minimum, theta = fittedModel$estimate, gradient = fittedModel$gradient,
+                            est_beta = BETA,
+                            est_beta_sd = est_sd[1],
+                            est_scale_horizontal = SCALE_HORIZONTAL,
+                            est_scale_vertical = SCALE_VERTICAL,
+                            est_scale_vertical_sd = est_sd[2],
+                            est_a1 = A1,
+                            est_a1_sd = est_sd[3],
+                            est_b1 = B1,
+                            est_b1_sd = est_sd[4],
+                            est_c1_coef = C1_coef,
+                            est_c1_coef_sd = est_sd[4 + 1:nb1],
+                            est_d1 = D1,
+                            est_a2 = A2,
+                            est_a2_sd = est_sd[4 + nb1 + 1],
+                            est_b2 = B2,
+                            est_b2_sd = est_sd[4 + nb1 + 2],
+                            est_c2_coef = C2_coef,
+                            est_c2_coef_sd = est_sd[4 + nb1 + 2 + 1:nb2],
+                            est_d2 = D2,
+                            splines_degree = splines_degree, inner_knots1 = inner_knots1, inner_knots2 = inner_knots2)
+
+          }else if(!d1_fix & !d2_fix){
+
+            results <- list(convergence_code = fittedModel$code, iterations = fittedModel$iterations, loglikelihood_value = -fittedModel$minimum, theta = fittedModel$estimate, gradient = fittedModel$gradient,
+                            est_beta = BETA,
+                            est_beta_sd = est_sd[1],
+                            est_scale_horizontal = SCALE_HORIZONTAL,
+                            est_scale_vertical = SCALE_VERTICAL,
+                            est_scale_vertical_sd = est_sd[2],
+                            est_a1 = A1,
+                            est_a1_sd = est_sd[3],
+                            est_b1 = B1,
+                            est_b1_sd = est_sd[4],
+                            est_c1_coef = C1_coef,
+                            est_c1_coef_sd = est_sd[4 + 1:nb1],
+                            est_d1 = D1,
+                            est_d1_sd = est_sd[4 + nb1 + 1],
+                            est_a2 = A2,
+                            est_a2_sd = est_sd[4 + nb1 + 2],
+                            est_b2 = B2,
+                            est_b2_sd = est_sd[4 + nb1 + 3],
+                            est_c2_coef = C2_coef,
+                            est_c2_coef_sd = est_sd[4 + nb1 + 3 + 1:nb2],
+                            est_d2 = D2,
+                            est_d1_sd = est_sd[4 + nb1 + 3 + nb2 + 1],
+                            splines_degree = splines_degree, inner_knots1 = inner_knots1, inner_knots2 = inner_knots2)
+
+          }
+
+        }
+
+      }
+
     }
 
   }else if(beta_fix){
--- R/transformParams.R
+++ R/transformParams.R
@@ -238,6 +238,118 @@ transformParams <- function(theta, init_beta,
 
       }
 
+    }else if(scale_horizontal_fix & !scale_vertical_fix){
+
+      SCALE_HORIZONTAL <- init_scale_horizontal
+      SCALE_VERTICAL <- exp(theta[2]) * vertical_scale_scaling
+
+      if(a1_fix & b1_fix & a2_fix & b2_fix){
+
+        A1 <- init_a1
+        B1 <- init_b1
+        A2 <- init_a2
+        B2 <- init_b2
+
+        if(c1_fix & c2_fix){
+
+          C1_coef <- init_c1_coef
+          C2_coef <- init_c2_coef
+
+          if(d1_fix & d2_fix){
+
+            D1 <- init_d1
+            D2 <- init_d2
+
+          }else if(!d1_fix & !d2_fix){
+
+            D1 <- theta[3] * d1_scaling
+            D2 <- theta[4] * d2_scaling
+
+          }
+
+        }else if(!c1_fix & !c2_fix){
+
+          if(d1_fix & d2_fix){
+
+            C1_coef <- theta[2 + 1:nb1] * c1_coef_scaling
+            D1 <- init_d1
+
+            C2_coef <- theta[2 + nb1 + 1:nb2] * c2_coef_scaling
+            D2 <- init_d2
+
+          }else if(!d1_fix & !d2_fix){
+
+            C1_coef <- theta[2 + 1:nb1] * c1_coef_scaling
+            D1 <- theta[2 + nb1 + 1] * d1_scaling
+
+            C2_coef <- theta[2 + nb1 + 1 + 1:nb2] * c2_coef_scaling
+            D2 <- theta[2 + nb1 + 1 + nb2 + 1] * d2_scaling
+
+          }
+
+        }
+
+      }else if(!a1_fix & !b1_fix & !a2_fix & !b2_fix){
+
+        if(c1_fix & c2_fix){
+
+          C1_coef <- init_c1_coef
+          C2_coef <- init_c2_coef
+
+          if(d1_fix & d2_fix){
+
+            A1 <- exp(theta[3]) * a1_scaling
+            B1 <- theta[4] * b1_scaling
+            D1 <- init_d1
+
+            A2 <- theta[5] * a2_scaling
+            B2 <- theta[6] * b2_scaling
+            D2 <- init_d2
+
+          }else if(!d1_fix & !d2_fix){
+
+            A1 <- exp(theta[3]) * a1_scaling
+            B1 <- theta[4] * b1_scaling
+            D1 <- theta[5] * d1_scaling
+
+            A2 <- theta[6] * a2_scaling
+            B2 <- theta[7] * b2_scaling
+            D2 <- theta[8] * d2_scaling
+
+          }
+
+        }else if(!c1_fix & !c2_fix){
+
+          if(d1_fix & d2_fix){
+
+            A1 <- exp(theta[3]) * a1_scaling
+            B1 <- theta[4] * b1_scaling
+            C1_coef <- theta[4 + 1:nb1] * c1_coef_scaling
+            D1 <- init_d1
+
+            A2 <- theta[4 + nb1 + 1] * a2_scaling
+            B2 <- theta[4 + nb1 + 2] * b2_scaling
+            C2_coef <- theta[4 + nb1 + 2 + 1:nb2] * c2_coef_scaling
+            D2 <- init_d2
+
+          }else if(!d1_fix & !d2_fix){
+
+            A1 <- exp(theta[3]) * a1_scaling
+            B1 <- theta[4] * b1_scaling
+            C1_coef <- theta[4 + 1:nb1] * c1_coef_scaling
+            D1 <- theta[4 + nb1 + 1] * d1_scaling
+
+            A2 <- theta[4 + nb1 + 2] * a2_scaling
+            B2 <- theta[4 + nb1 + 3] * b2_scaling
+            C2_coef <- theta[4 + nb1 + 3 + 1:nb2] * c2_coef_scaling
+            D2 <- theta[4 + nb1 + 3 + nb2 + 1] * d1_scaling
+
+          }
+
+        }
+
+      }
+
     }
 
   }else if(beta_fix){
