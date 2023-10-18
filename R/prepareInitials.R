82b1e81ef0dbd2b168b8d621419637bd8978d4f7
geostatistech <yourlainess@gmail.com>
2023-10-18 09:32:37 +0300

    added feature where scale horizontal can be fixed and only scale vertical is estimated

--- R/prepareInitials.R
+++ R/prepareInitials.R
@@ -188,6 +188,50 @@ prepareInitials <- function(init_beta,
       }else{
         stop("a1, b1, a2, and b2 must either all be FIXED or ESTIMATED. Try again.")
       }
+    }else if(scale_horizontal_fix & !scale_vertical_fix){
+      if(a1_fix & b1_fix & a2_fix & b2_fix){
+        if(c1_fix & c2_fix){
+          if(d1_fix & d2_fix){
+            theta0 <- c(init_scale_vertical)
+          }else if(!d1_fix & !d2_fix){
+            theta0 <- c(init_scale_vertical, init_d1, init_d2)
+          }else{
+            stop("d1 and d2 must either both be FIXED or ESTIMATED. Try again.")
+          }
+        }else if(!c1_fix & !c2_fix){
+          if(d1_fix & d2_fix){
+            theta0 <- c(init_scale_vertical, init_c1_coef, init_c2_coef)
+          }else if(!d1_fix & !d2_fix){
+            theta0 <- c(init_scale_vertical, init_c1_coef, init_d1, init_c2_coef, init_d2)
+          }else{
+            stop("d1 and d2 must either both be FIXED or ESTIMATED. Try again.")
+          }
+        }else{
+          stop("c1 and c2 must either both be FIXED or ESTIMATED. Try again.")
+        }
+      }else if(!a1_fix & !b1_fix & !a2_fix & !b2_fix){
+        if(c1_fix & c2_fix){
+          if(d1_fix & d2_fix){
+            theta0 <- c(init_scale_vertical, init_a1, init_b1, init_a2, init_b2)
+          }else if(!d1_fix & !d2_fix){
+            theta0 <- c(init_scale_vertical, init_a1, init_b1, init_d1, init_a2, init_b2, init_d2)
+          }else{
+            stop("d1 and d2 must either both be FIXED or ESTIMATED. Try again.")
+          }
+        }else if(!c1_fix & !c2_fix){
+          if(d1_fix & d2_fix){
+            theta0 <- c(init_scale_vertical, init_a1, init_b1, init_c1_coef, init_a2, init_b2, init_c2_coef)
+          }else if(!d1_fix & !d2_fix){
+            theta0 <- c(init_scale_vertical, init_a1, init_b1, init_c1_coef, init_d1, init_a2, init_b2, init_c2_coef, init_d2)
+          }else{
+            stop("d1 and d2 must either both be FIXED or ESTIMATED. Try again.")
+          }
+        }else{
+          stop("c1 and c2 must either both be FIXED or ESTIMATED. Try again.")
+        }
+      }else{
+        stop("a1, b1, a2, and b2 must either all be FIXED or ESTIMATED. Try again.")
+      }
     }else{
       stop("scale horizontal and scale vertical must either both be FIXED or ESTIMATED. Try again.")
     }
